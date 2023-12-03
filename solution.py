1f754ab8e64cc811964ad2eb18501401699b1535
LuisAvila52 <la6303379@gmail.com>
2023-12-03 12:06:20 -0500

    Modif

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 from typing import Tuple
 
-
 db = set()
-
-
 def almacenar_orden(orden: Tuple[str, ...]) -> bool:
-    pass
+    if orden in db:
+        return False
+    else:
+        db.add(orden)
+    return True
